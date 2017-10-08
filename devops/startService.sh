b2408adf9433c1e61146d0650ba76fccad1d5488
Wang pei <519875872@qq.com>
2017-10-08 16:39:14 +0800

    remove useless file

--- devops/startService.sh
+++
@@ -1,1 +0,0 @@
-nohup java -jar ./build/libs/emailService-0.0.1-SNAPSHOT.jar &
--- devops/stopService.sh
+++
@@ -1,5 +0,0 @@
-pid=`ps -ef|grep java | grep -v 'grep'|grep -v 'jenkins'|awk '{print $2}'`
-
-echo $pid
-
-kill -9 $pid
