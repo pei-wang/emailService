a90474c2da3e03fc5cbf0b0391cd9adfecd72fa4
Wang pei <519875872@qq.com>
2017-10-09 11:20:13 +0800

    add swaggerˆ

--- src/main/java/com/pei/api/emailService/EmailServiceApplication.java
+++ src/main/java/com/pei/api/emailService/EmailServiceApplication.java
@@ -2,7 +2,6 @@ package com.pei.api.emailService;
 
 import com.pei.api.emailService.Configuration.EmailConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
--- src/main/java/com/pei/api/emailService/controllers/MailController.java
+++ src/main/java/com/pei/api/emailService/controllers/MailController.java
@@ -4,7 +4,6 @@ import com.pei.api.emailService.Configuration.EmailConfiguration;
 import com.pei.api.emailService.entity.EmailMessage;
 import com.pei.api.emailService.services.SimpleEmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +22,5 @@ public class MailController {
     public String sendEmail(@RequestBody EmailMessage emailMessage) throws MessagingException {
         simpleEmailSenderService.send(emailMessage);
         return "hello world";
-    }
-
-    @GetMapping
-    public String test() {
-        return "test" + emailConfiguration.getAuth() + emailConfiguration.getSslEnable();
     }
 }
